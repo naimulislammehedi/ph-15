2039de71c06b6bd9ddc44336b8853255843d593a
Mehedi <mehedi.interface@gmail.com>
2026-04-19 10:16:45 +0600

    15_3 Problem solving with While Loop

---
+++ event.js
@@ -0,0 +1,15 @@
+let num = 1; 
+// while (num <= 10) {
+//     console.log(num); 
+//     num++; 
+// }
+
+while (num <= 10) {
+    // console.log(num); 
+    if (num % 2 === 0) {
+        console.log("Even number:", num); 
+    } else {
+        console.log("Odd number: ", num); 
+    }
+    num++; 
+}
---
+++ range.js
@@ -0,0 +1,11 @@
+let num = 1; 
+
+// while (num < 10) {
+//     console.log(num); 
+//     num++; 
+// }
+
+while (num <= 100) {
+    console.log(num); 
+    num++; 
+}
