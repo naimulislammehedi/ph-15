d094c4edbecccde5580dbe49528e600f8591b918
Mehedi <mehedi.interface@gmail.com>
2026-04-19 10:46:18 +0600

    15_4 Introduction to For Loop

---
+++ for_loop.js
@@ -0,0 +1,13 @@
+let num = 0; 
+
+while (num < 5) {
+    console.log(num); 
+    num++; 
+}
+
+// syntax 
+// for (first part; second part; third part); 
+// for (loop variale declaration; loop condition); 
+for (let i = 0; i < 5; i++) {
+    console.log(i); 
+}
---
+++ odd_even.js
@@ -0,0 +1,10 @@
+// Prints all even numbers from 0 to 9
+for (let i = 0; i < 10; i=i+2) {
+    console.log(i); 
+}
+
+// Prints all odd numbers from 1 to 9
+for (let i = 1; i < 10; i=i+2) {
+    console.log(i); 
+}
+
