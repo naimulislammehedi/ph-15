18de61875aeeb712c08cb88cb067454ef1fbe4b5
Mehedi <mehedi.interface@gmail.com>
2026-04-24 11:17:21 +0600

    odd even while

---
+++ odd_even_while.js
@@ -0,0 +1,24 @@
+// Find all the odd numbers from 61 to 100.
+
+// Odd Numbers 
+let oddNumbers = 61; 
+
+while (oddNumbers <= 100) {
+    if (oddNumbers % 2 !== 0) {
+        console.log(oddNumbers); 
+    }
+    oddNumbers += 2; 
+}
+
+
+// Find all the even numbers from 78 to 98.
+
+let evenNumbers = 78; 
+
+while (evenNumbers <= 98) {
+    if (evenNumbers % 2 === 0) {
+        console.log(evenNumbers); 
+    }
+    evenNumbers += 2; 
+}
+
