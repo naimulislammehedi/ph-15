3276b01fb9e0824f95d92357a277cfdad6a372da
Mehedi <mehedi.interface@gmail.com>
2026-04-24 11:08:26 +0600

    commitment.js

--- commitment.js
+++ commitment.js
@@ -2,6 +2,23 @@
 "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
 
  */
-for (let i = 1; i <= 60; i++) {
+// for (let i = 1; i <= 60; i++) {
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!")
+// }
+
+// const numbers = 0; 
+
+// while (num < 5) {
+//     console.log("looping", numbers)
+//     // num = num + 1; 
+//     // num += 1; 
+//     num ++; 
+
+// } 
+
+let i = 0; 
+
+while (i <= 60) {
     console.log("I will invest at least 6 hrs every single day for next 60 days!")
+    i++;
 }
