d76a63bdf6c647492bfcda52110f8460f6ba9a49
Mehedi <mehedi.interface@gmail.com>
2026-04-24 11:39:13 +0600

    multiplication_table_while.js

---
+++ multiplication_table_while.js
@@ -0,0 +1,9 @@
+// As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+let i = 1; 
+
+while (i <= 10) {
+    // console.log(i); 
+    console.log(`5 x ${i} = ${5 * i}`)
+    i++; 
+}
